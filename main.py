e988194247074eb46070d74892dcad3ecbc4040d
pseudolonewolf <skulblaka71@gmail.com>
2025-03-28 14:15:25 -0400

    combat framework update. You can die now!

--- main.py
+++ main.py
@@ -53,7 +53,7 @@ def main() -> None:
         title=title,
         vsync=True,
     ) as context:
-        root_console = tcod.Console(screen_width, screen_height, order="F")
+        root_console = tcod.console.Console(screen_width, screen_height, order="F")
         while True:
             engine.render(console=root_console, context=context)
 

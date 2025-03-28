b8640f433f7f6ef728739bee983484cb0cf0c1ff
pseudolonewolf <skulblaka71@gmail.com>
2025-03-28 00:28:06 -0400

    created basic entity framework

--- input_handlers.py
+++ input_handlers.py
@@ -2,7 +2,7 @@ from typing import Optional
 
 import tcod.event
 
-from actions import Action, EscapeAction, MovementAction
+from actions import Action, EscapeAction, BumpAction
 
 class EventHandler(tcod.event.EventDispatch[Action]):
     def ev_quit(self, event: tcod.event.Quit) -> Optional[Action]:
@@ -13,24 +13,24 @@ class EventHandler(tcod.event.EventDispatch[Action]):
 
         key = event.sym
         if key == tcod.event.KeySym.KP_8: # Up
-            action = MovementAction(dx=0, dy=-1)
+            action = BumpAction(dx=0, dy=-1)
         elif key == tcod.event.KeySym.KP_2: # Down
-            action = MovementAction(dx=0, dy=1)
+            action = BumpAction(dx=0, dy=1)
         elif key == tcod.event.KeySym.KP_4: # Left
-            action = MovementAction(dx=-1, dy=0)
+            action = BumpAction(dx=-1, dy=0)
         elif key == tcod.event.KeySym.KP_6: # Right
-            action = MovementAction(dx=1, dy=0)
+            action = BumpAction(dx=1, dy=0)
         elif key == tcod.event.KeySym.KP_7: # Up-Left
-            action = MovementAction(dx=-1, dy=-1)
+            action = BumpAction(dx=-1, dy=-1)
         elif key == tcod.event.KeySym.KP_9: # Up-Right
-            action = MovementAction(dx=1, dy=-1)
+            action = BumpAction(dx=1, dy=-1)
         elif key == tcod.event.KeySym.KP_1: # Down-Left
-            action = MovementAction(dx=-1, dy=1)
+            action = BumpAction(dx=-1, dy=1)
         elif key == tcod.event.KeySym.KP_3: # Down-Right
-            action = MovementAction(dx=1, dy=1)
+            action = BumpAction(dx=1, dy=1)
 
         elif key == tcod.event.KeySym.KP_5: # Wait
-            action = MovementAction(dx=0, dy=0)
+            action = BumpAction(dx=0, dy=0)
 
         elif key == tcod.event.K_ESCAPE:
             action = EscapeAction()
